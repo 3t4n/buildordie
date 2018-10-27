1a4df03e89dd867dfd5608dda0f32e0773eca5eb
JavierMacM <43215688+JavierMacM@users.noreply.github.com>
2018-10-27 13:18:41 -0500

    tile

--- src/buildordie/Tile.java
+++ src/buildordie/Tile.java
@@ -8,19 +8,22 @@ public class Tile
 	private int anchura, altura;
 	private BufferedImage piso;
 	
-	public Tile(Point p, int w, int h){
+	public Tile(Point p, int w, int h)
+	{
 		posicion = p;
 		anchura = w;
 		altura = h;
 		//piso = Utils.loadImage(d);
 	}
 	
-	public void update(){
+	public void update()
+	{
 		Point off = Global.jugador.getPosition();
 		posicion = new Point(off.getX()+posicion.getX(), off.getY()+posicion.getY());
 	}
 	
-	public void render(Graphics g){
+	public void render(Graphics g)
+	{
 		g.setColor(Color.black);
 		g.fillRect(point.getX(), point.getY(), anchura, altura);
 		
