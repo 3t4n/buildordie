e17750729de124a6e8122418161dcab24e1c48ec
barive <43215251+barive@users.noreply.github.com>
2018-10-27 13:18:17 -0500

    proto 2 mapa

--- src/buildordie/Map.java
+++ src/buildordie/Map.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 public class Map
 {
 	private ArrayList<ArrayList<Tile>> tiles;
-	public Map(int xt, int yt, int dimensionx, int dimensiony)
-	{
+	public Map(int xt, int yt, int dimensionx, int dimensiony){
 		tiles = new ArrayList<ArrayList<Tile>>();
 		for(int i = 0; i < yt; i++){
 			ArrayList<Tile> fila = new ArrayList<ArrayList<Tile>>();
@@ -14,6 +13,22 @@ public class Map
 				fila.push(new Tile(new Point(x*dimensionx,y*dimensiony), dimensionx, dimensiony);
 			}
 			tiles.push(fila);
+		}
+	}
+	
+	public void update(){
+		for(int i = 0; i < yt; i++){
+			for(int j = 0; j < xt; j++){
+				tiles.get(i).get(j).update();
+			}
+		}
+	}
+	
+	public void render(Graphics g){
+		for(int i = 0; i < yt; i++){
+			for(int j = 0; j < xt; j++){
+				tiles.get(i).get(j).render(g);
+			}
 		}
 	}
 }
