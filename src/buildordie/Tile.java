0f87dab87619ab4eaa62ea7c3dde162fcbbc060a
JavierMacM <43215688+JavierMacM@users.noreply.github.com>
2018-11-03 09:40:05 -0600

    Update src/buildordie/Tile.java

--- src/buildordie/Tile.java
+++ src/buildordie/Tile.java
@@ -9,7 +9,7 @@ public class Tile
 {
 	private Point posicion,offset;
 	private int anchura, altura;
-	
+
 	public Tile(Point p, int w, int h){
 		posicion = p;
 		anchura = w;
@@ -17,17 +17,22 @@ public class Tile
 		//piso = Utils.loadImage(d);
 		offset = new Point(0,0);
 	}
-	
+
 	public void update(){
 		offset = Global.jugador.getPosition();
+
 	//	posicion = new Point((int)(off.getX()+posicion.getX()), (int)(off.getY()+posicion.getY()));
+	}
+
+	public Rectangle getRectangle(){
+		return new Rectangle((int)(posicion.getX()-offset.getX()), (int)(posicion.getY()-offset.getY()), anchura, altura);
 	}
 	
 	public void render(Graphics g){
 	//	BufferedImage piso = new BufferedImage(anchura,altura,BufferedImage.TYPE_INT_RGB);
 	//	Graphics t = piso.getGraphics();
 		g.setColor(Color.black);
-		g.fillRect((int)(posicion.getX()-offset.getX()), (int)(posicion.getY()-offset.getY()), anchura, altura);	
+		g.fillRect((int)(posicion.getX()-offset.getX()), (int)(posicion.getY()-offset.getY()), anchura, altura);
 		g.setColor(Color.white);
 		g.fillRect((int)(posicion.getX()-offset.getX()), (int)(posicion.getY()-offset.getY()), anchura-1, altura-1);
 	}
